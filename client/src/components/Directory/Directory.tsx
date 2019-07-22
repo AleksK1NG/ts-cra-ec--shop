14727567d8e445cd36135063c0a9a26a5c05dbb5
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-22 14:09:30 +0300

    add Client Directory component

--- client/src/components/Directory/Directory.tsx
+++ client/src/components/Directory/Directory.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './Directory.styles.scss'
 
-interface IProps {
+import { IDSection, sections } from './sections'
+import MenuItem from '../Menu/MenuItem/MenuItem'
 
-}
+interface IProps {}
 
 const Directory: React.FC<IProps> = () => {
   return (
     <div className="directory-menu">
-
+      {sections.map(({ id, title, imageUrl, size }: IDSection) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      ))}
     </div>
   )
 }
