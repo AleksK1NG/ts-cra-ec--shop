94b91d1cd5876e473fe64193b2ecd6d3a776e954
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-22 17:56:15 +0300

    add Client Shop module getAllCategories

--- client/src/store/modules/shop/shopSaga.ts
+++ client/src/store/modules/shop/shopSaga.ts
@@ -1,20 +1,34 @@
-import { put, all, takeLatest } from 'redux-saga/effects';
-// import ApiService from '../../../services/apiService';
-import axios from 'axios';
-import { getPostsError, getPostsSuccess } from './shopActions';
-import { shopActions } from './types';
+import { put, all, takeLatest, call } from 'redux-saga/effects'
+import ApiService from '../../../services/apiService'
+import axios from 'axios'
+import { getCategoriesError, getCategoriesSuccess, getPostsError, getPostsSuccess } from './shopActions'
+import { GET_ALL_CATEGORIES_REQUEST, shopActions } from './types'
 
 export function* getPostsSaga(): any {
   try {
-    const { data } = yield axios.get('https://jsonplaceholder.typicode.com/posts');
-    debugger;
-    yield put(getPostsSuccess(data));
+    const { data } = yield axios.get('https://jsonplaceholder.typicode.com/posts')
+    debugger
+    yield put(getPostsSuccess(data))
   } catch (error) {
-    console.error(error);
-    yield put(getPostsError(error));
+    console.error(error)
+    yield put(getPostsError(error))
+  }
+}
+
+export function* getAllCategoriesSaga(): any {
+  try {
+    const { data } = yield call(ApiService.getAllCategories)
+    debugger
+    yield put(getCategoriesSuccess(data))
+  } catch (error) {
+    console.error(error)
+    yield put(getCategoriesError(error))
   }
 }
 
 export function* saga() {
-  yield all([takeLatest(shopActions.GET_POSTS_REQUEST, getPostsSaga)]);
+  yield all([
+    takeLatest(shopActions.GET_POSTS_REQUEST, getPostsSaga),
+    takeLatest(GET_ALL_CATEGORIES_REQUEST, getAllCategoriesSaga)
+  ])
 }
