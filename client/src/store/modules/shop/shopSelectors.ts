a10e26b8b75f9845159b9d13696e94f3cbd4ed35
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-22 13:26:33 +0300

    init project

--- client/src/store/modules/shop/shopSelectors.ts
+++ client/src/store/modules/shop/shopSelectors.ts
@@ -1,18 +1,18 @@
-import { createSelector } from 'reselect';
-import { IShop } from './shopReducer';
+import { createSelector } from 'reselect'
+import { IShop } from './shopReducer'
 
 // Selectors
 
-const moduleName = 'shop';
+const moduleName = 'shop'
 
-export const stateSelector = (state: any) => state.shop;
+export const stateSelector = (state: any) => state[moduleName]
 
 export const alexNameSelector = createSelector(
   stateSelector,
   (state: IShop) => state.name
-);
+)
 
 export const allPostsSelector = createSelector(
   stateSelector,
   (state: IShop) => state.posts
-);
+)
