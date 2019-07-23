12ff30b193aae025796535953368aa2cf41a6916
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-23 15:37:12 +0300

    add FormInput component

---
+++ client/src/components/SingIn/SignIn.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import './SignIn.styles.scss'
+
+const SignIn: React.FC = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignIn
