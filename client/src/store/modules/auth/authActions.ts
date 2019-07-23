5a346fa252440bb958438c6a49fcccc10e94a24e
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-23 18:36:25 +0300

    add Auth store module actions, reducer, types

---
+++ client/src/store/modules/auth/authActions.ts
@@ -0,0 +1,44 @@
+import { AuthAction, AuthActionTypes } from './types'
+import { IUser } from '../../../models/models'
+
+export const loginRequest = (): AuthAction => ({
+  type: AuthActionTypes.LOGIN_USER_REQUEST
+})
+
+export const loginSuccess = (user: IUser): AuthAction => ({
+  type: AuthActionTypes.LOGIN_USER_REQUEST,
+  payload: { user }
+})
+
+export const loginError = (error: any): AuthAction => ({
+  type: AuthActionTypes.LOGIN_USER_REQUEST,
+  payload: { error }
+})
+
+export const registerRequest = (): AuthAction => ({
+  type: AuthActionTypes.REGISTER_USER_REQUEST
+})
+
+export const registerSuccess = (user: IUser): AuthAction => ({
+  type: AuthActionTypes.REGISTER_USER_SUCCESS,
+  payload: { user }
+})
+
+export const registerError = (error: any): AuthAction => ({
+  type: AuthActionTypes.REGISTER_USER_ERROR,
+  payload: { error }
+})
+
+export const loadUserRequest = (): AuthAction => ({
+  type: AuthActionTypes.LOAD_USER_REQUEST
+})
+
+export const loadUserSuccess = (user: IUser): AuthAction => ({
+  type: AuthActionTypes.LOAD_USER_SUCCESS,
+  payload: { user }
+})
+
+export const loadUserError = (error: any): AuthAction => ({
+  type: AuthActionTypes.LOAD_USER_ERROR,
+  payload: { error }
+})
