2a921eceeee3b5bd3267d889be7618e2895cc428
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-23 16:18:12 +0300

    add SignIn form

--- client/src/components/Shared/FormInput/FormInput.tsx
+++ client/src/components/Shared/FormInput/FormInput.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './FormInput.styles.scss'
 
 interface IProps {
-  handleChange: () => void
-  otherProps: any
+  handleChange: (e: any) => void
+  otherProps?: any
   label: string
+  name: string
+  value: string
+  type?: string
+  required?: boolean
 }
 
-const FormInput: React.FC<IProps> = ({ handleChange, otherProps, label }) => {
+const FormInput: React.FC<IProps & React.HTMLProps<HTMLInputElement>> = ({ handleChange, label, ...otherProps }) => {
   return (
     <div className="group">
       <input className="form-input" onChange={handleChange} {...otherProps} />
