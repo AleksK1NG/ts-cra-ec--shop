301dc927833b186da92c97b48625990199c4faf1
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-23 15:29:31 +0300

    add SignInAndSignUpPage

---
+++ client/src/pages/SignInAndSignUpPage/SignInAndSignUpPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SignInAndSignUpPage.styles.scss'
+
+const SignInAndSignUpPage: React.FC = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+
+    </div>
+  )
+}
+
+export default SignInAndSignUpPage
