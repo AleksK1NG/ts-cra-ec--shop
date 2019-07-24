82ecf7d2822800f033b802e659f285d3b11d6f43
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-24 16:31:09 +0300

    add CheckoutItem

---
+++ client/src/components/Checkout/CheckoutItem/CheckoutItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { ICollectionItem } from '../../../models/models'
+import './CheckoutItem.styles.scss'
+
+interface IProps {
+  cartItem: ICollectionItem
+}
+
+const CheckoutItem: React.FC<IProps> = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button">&#10005;</div>
+    </div>
+  )
+}
+
+export default CheckoutItem
