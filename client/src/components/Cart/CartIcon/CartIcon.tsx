e2c92b152856aedeb3c501bd18a1628df16517df
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-24 18:09:45 +0300

    add CartIcon items count selector

--- client/src/components/Cart/CartIcon/CartIcon.tsx
+++ client/src/components/Cart/CartIcon/CartIcon.tsx
@@ -5,21 +5,26 @@ import { connect } from 'react-redux'
 
 import { ReactComponent as ShoppingIcon } from '../../../assets/shopping-bag.svg'
 import { toggleCartHidden } from '../../../store/modules/UI/uiActions'
+import { AppState } from '../../../store/rootReducer'
+import { cartItemsCountSelector } from '../../../store/modules/cart/cartSelectors'
 
 interface IProps {
   toggleCartHidden: () => void
+  itemCount: number
 }
 
-const CartIcon: React.FC<IProps> = ({ toggleCartHidden }) => {
+const CartIcon: React.FC<IProps> = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   )
 }
 
 export default connect(
-  null,
+  (state: AppState) => ({
+    itemCount: cartItemsCountSelector(state)
+  }),
   { toggleCartHidden }
 )(CartIcon)
