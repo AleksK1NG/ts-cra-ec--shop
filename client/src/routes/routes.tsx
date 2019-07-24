1d30b59420398fed8ab3881528b6a7c13da93346
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-24 14:22:08 +0300

    add Routes auth redirect if user logged in

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { AppState } from '../store/rootReducer'
+import { userSelector } from '../store/modules/auth/authSelectors'
+import { IUser } from '../models/models'
 
 const HomePage = React.lazy(() => import('../pages/HomePage/HomePage'))
 const ShopPage = React.lazy(() => import('../pages/ShopPage/ShopPage'))
 const SignInAndSignUpPage = React.lazy(() => import('../pages/SignInAndSignUpPage/SignInAndSignUpPage'))
 
-const Routes = () => {
+interface IProps {
+  user: IUser | null
+}
+
+const Routes: React.FC<IProps> = ({ user }) => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
-      <Route path="/signin" component={SignInAndSignUpPage} />
+      <Route path="/signin" render={() => (user ? <Redirect to="/" /> : <SignInAndSignUpPage />)} />
     </Switch>
   )
 }
 
-export default Routes
+export default connect(
+  (state: AppState) => ({
+    user: userSelector(state)
+  }),
+  null
+)(Routes)
