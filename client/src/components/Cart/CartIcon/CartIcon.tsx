4966f2ce400e1a2699976488bea2cfd8e7dd1161
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-24 14:40:45 +0300

    add CartIcon component

--- client/src/components/Cart/CartIcon/CartIcon.tsx
+++ client/src/components/Cart/CartIcon/CartIcon.tsx
@@ -3,13 +3,23 @@ import React from 'react'
 import './CartIcon.styles.scss'
 import { connect } from 'react-redux'
 
-interface IProps {}
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+import { toggleCartHidden } from '../../../store/modules/UI/uiActions'
 
-const CartIcon: React.FC<IProps> = () => {
-  return <div></div>
+interface IProps {
+  toggleCartHidden: () => void
+}
+
+const CartIcon: React.FC<IProps> = ({ toggleCartHidden }) => {
+  return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">0</span>
+    </div>
+  )
 }
 
 export default connect(
   null,
-  {}
+  { toggleCartHidden }
 )(CartIcon)
