06429406d4fdf1f7f204a1d8b47a2cc33f5cecf2
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-25 13:30:06 +0300

    add Directory component connect data

--- client/src/components/Directory/Directory.tsx
+++ client/src/components/Directory/Directory.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import MenuItem from '../Menu/MenuItem/MenuItem'
+import { AppState } from '../../store/rootReducer'
+import { sectionsSelector } from '../../store/modules/directory/directorySelector'
+import { ISection } from '../../store/modules/directory/mockData'
+
 import './Directory.styles.scss'
 
-import { IDSection, sections } from './sections'
-import MenuItem from '../Menu/MenuItem/MenuItem'
+interface IProps {
+  sections: ISection[]
+}
 
-interface IProps {}
-
-const Directory: React.FC<IProps> = () => {
+const Directory: React.FC<IProps> = ({ sections }) => {
   return (
     <div className="directory-menu">
-      {sections.map(({ id, title, imageUrl, size, linkUrl }: IDSection) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}  />
+      {sections.map(({ id, title, imageUrl, size, linkUrl }: ISection) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
       ))}
     </div>
   )
 }
 
-export default Directory
+export default connect(
+  (state: AppState) => ({
+    sections: sectionsSelector(state)
+  }),
+  null
+)(Directory)
