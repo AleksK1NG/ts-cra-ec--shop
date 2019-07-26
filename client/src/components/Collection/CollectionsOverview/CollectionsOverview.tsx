6c993e9758ea9f2579d1fe23504dd2306b941010
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-07-26 17:44:03 +0300

    add styled CollectionsOverview

--- client/src/components/Collection/CollectionsOverview/CollectionsOverview.tsx
+++ client/src/components/Collection/CollectionsOverview/CollectionsOverview.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import './CollectionsOverview.styles.scss'
 import { AppState } from '../../../store/rootReducer'
 import { collectionsSelector } from '../../../store/modules/shop/shopSelectors'
 import { ISDataCollection } from '../../../store/modules/shop/shopData'
 import CollectionPreview from '../CollectionPreview/CollectionPreview'
+
+import { CollectionsOverviewContainer } from './CollectionsOverview.styles'
 
 interface IProps {
   collections: ISDataCollection[]
@@ -12,11 +13,11 @@ interface IProps {
 
 const CollectionsOverview: React.FC<IProps> = ({ collections }) => {
   return (
-    <div className="collections-overview">
+    <CollectionsOverviewContainer>
       {collections.map(({ id, ...otherCollectionProps }: ISDataCollection) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
-    </div>
+    </CollectionsOverviewContainer>
   )
 }
 
